9c19e7fcc8444f5c67fb77375859ddc03c954628
BuildTools <richard.mazanek@gmail.com>
2020-11-29 10:28:15 -0500

    First commit

---
+++ Assets/Scripts/NumberWizardUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizardUI : MonoBehaviour
+{
+
+    [SerializeField] int guessMin;
+    [SerializeField] int guessMax;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+    
+    void Start()
+    {
+        StartGame();
+    }
+    
+    public void StartGame()
+    {
+        guess = (guessMax  + guessMin) / 2;
+        guessText.text = guess.ToString();
+        guessMax = guessMax + 1;
+    }
+    
+    public void OnPressHigher()
+    {
+        guessMin = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        guessMax = guess;
+        NextGuess();
+    }
+    
+    void NextGuess()
+    {
+        guess = (guessMax  + guessMin) / 2;
+        
+        if (guess == 1000)
+        {
+            guessMin = 1000;
+        }
+        
+        guessText.text = guess.ToString();
+    }
+}
